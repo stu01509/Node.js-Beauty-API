4edbdba69ede94c12b355a1878d4ca5f55c03e80
Cliff Su <stu01509@yahoo.com.tw>
2019-07-27 00:08:47 +0800

    fix: accept ptt over18 rule to crawler page.

--- crawler/ptt.js
+++ crawler/ptt.js
@@ -1,4 +1,4 @@
-const request = require('request');
+let request = require('request');
 const cheerio = require('cheerio');
 
 const BEAUTY_URL = 'https://www.ptt.cc/bbs/Beauty/';
@@ -8,6 +8,31 @@ const PostSchema = require('../models/Post');
 const beautyOptions = {
   url: BEAUTY_URL,
   method: 'GET',
+};
+
+
+const acceptData = {
+  from: '/bbs/Beauty/index.html',
+  yes: 'yes',
+};
+
+const acceptOver18 = () => {
+  // 同意我已滿 18 歲
+  const over18Cookie = request.jar();
+  // 將 over18Cookie 套用在所有的 Request上
+  request = request.defaults({
+    jar: over18Cookie,
+  });
+
+  request.post({
+    url: 'https://www.ptt.cc/ask/over18',
+    form: acceptData,
+  }, (acceptErr, acceptRes, acceptBody) => {
+    // 同意後會收到 Status Code 302
+    if (acceptErr || acceptRes.statusCode !== 302) {
+      return;
+    }
+  });
 };
 
 // 抓取貼文內容
@@ -60,6 +85,7 @@ const getBeautyPostLink = (id) => {
 
 // 抓取 PTT 看板的頁數連結
 const getBeautyTopLink = () => {
+  acceptOver18();
   request(beautyOptions, (err, res, body) => {
     if (err || res.statusCode !== 200) {
       return;
