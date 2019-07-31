2c461126274bf2ce8d79c11c53c1303413d9bd72
Cliff Su <stu01509@yahoo.com.tw>
2019-07-31 17:49:24 +0800

    feat: finished use joi to validate request query string format.

---
+++ libs/queryValidate.js
@@ -0,0 +1,30 @@
+const Joi = require('joi');
+
+const getAllValidate = Joi.object().keys({
+  skip: Joi.number().default(0).positive(),
+});
+
+const dcardValidate = Joi.object().keys({
+  query: Joi.string(),
+  sex: Joi.string().valid(['m', 'f', 'M', 'F']),
+  skip: Joi.number().default(0).positive(),
+  sort: Joi.string().default('new').valid(['new', 'old']),
+}).or('query', 'sex');
+
+const meteorValidate = Joi.object().keys({
+  query: Joi.string(),
+  sex: Joi.string().valid(['m', 'f', 'M', 'F']),
+  skip: Joi.number().default(0).positive(),
+  sort: Joi.string().default('new').valid(['new', 'old']),
+}).or('query', 'sex');
+
+const pttValidate = Joi.object().keys({
+  query: Joi.string(),
+  skip: Joi.number().default(0).positive(),
+  sort: Joi.string().default('now').valid(['new', 'old']),
+}).or('query');
+
+module.exports.getAllValidate = getAllValidate;
+module.exports.dcardValidate = dcardValidate;
+module.exports.meteorValidate = meteorValidate;
+module.exports.pttValidate = pttValidate;
